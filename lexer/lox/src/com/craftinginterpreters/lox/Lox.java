94ec32d2b89ca5d5aed07ff2fac14ff4994630fb
Sarthak_Kotia <sarthakkotia@gmail.com>
2024-12-21 17:12:41 +0530

    adding code for lexer, added initial functions, token implementation left

---
+++ lexer/lox/src/com/craftinginterpreters/lox/Lox.java
@@ -0,0 +1,79 @@
+package com.craftinginterpreters.lox;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
+
+public class Lox {
+    static boolean hadError = false;
+    public static void main(String[] args) throws IOException {
+        // args here refers to the additional arguments given upon running the main function
+        if(args.length > 1){
+            System.out.println("Usage: jlox [script]");
+            System.exit(64);
+            //   The exit code 64 corresponds to : The command was used incorrectly, e.g., with the
+            //			   wrong number of arguments, a bad flag, a bad syntax
+            //			   in a parameter, or whatever. defined in sysexits.h file in UNIX
+
+        } else if (args.length == 1) {
+            // Our interpreter supports two ways of running code.
+            // If you start jlox from the command line and give it a path to a file, it reads the file and executes it.
+            runFile(args[0]);
+        }else{
+            // If you want a more intimate conversation with your interpreter, you can also run it interactively.
+            // Fire up jlox without any arguments, and it drops you into a prompt where you can enter and execute code one line at a time.
+            runPrompt();
+        }
+    }
+    private static void runFile(String path) throws IOException{
+            byte[] bytes = Files.readAllBytes(Paths.get(path));
+            // read all the data in the file in the path defined as byte format so that theres no loss of data
+            // all of the source code is stores in the bytes array
+            run(new String(bytes, Charset.defaultCharset()));
+            // from the bytes array convert it back to source code as a String data type and then run that code
+            if(hadError) System.exit(65);
+            // The input data was incorrect in some way.  This
+            // should only be used for user's data and not system
+            // files.
+    }
+    private static void runPrompt() throws IOException{
+        // this is the prompt function of the interpreter where it would process each prompt line by line
+        // InputStreamReader converts bytes to stream of character save it to reader buffer to be read later
+        InputStreamReader input = new InputStreamReader(System.in);
+        BufferedReader reader = new BufferedReader(input);
+
+        while(true){
+            System.out.println("> ");
+            String line = reader.readLine();
+            if(line == null) break;
+            run(line);
+            //reset the variable in the interactive loop
+            hadError = false;
+        }
+    }
+    private static void run(String source){
+        Scanner scanner = new Scanner(source);
+
+        List<Token>tokens = scanner.scanTokens();
+        for(Token token: tokens){
+            System.out.println(token);
+        }
+    }
+    static void error(int line, String message){
+        report(line, "", message);
+    }
+    private static void report(int line, String where, String message){
+        System.err.println("[line "+line+"] Error"+where+": "+message);
+        // TODO: implement the report function as following:
+        // Error: Unexpected "," in argument list.
+        //
+        //     15 | function(first, second,);
+        //                               ^-- Here.
+        hadError = true;
+    }
+}
