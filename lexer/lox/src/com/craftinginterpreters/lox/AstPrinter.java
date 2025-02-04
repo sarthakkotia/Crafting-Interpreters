6131669963eb56cc98ec6f25bce331c79241c45c
Sarthak_Kotia <sarthakkotia@gmail.com>
2025-02-04 19:27:52 +0530

    tested AstPrinter class

--- lexer/lox/src/com/craftinginterpreters/lox/AstPrinter.java
+++ lexer/lox/src/com/craftinginterpreters/lox/AstPrinter.java
@@ -31,12 +31,27 @@ public class AstPrinter implements Expression.Visitor<String>{
 
     String parenthesize(String name, Expression[] expressions){
         StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append(" ( ");
+        stringBuilder.append(" ( ").append(name)    ;
         for(Expression expression: expressions){
             stringBuilder.append(" ");
             stringBuilder.append(print(expression));
         }
         stringBuilder.append(" ) ");
         return stringBuilder.toString();
+    }
+
+    public static void main(String[] args){
+        Expression expression = new Expression.Binary(
+                new Expression.Unary(
+                        new Token(TokenType.MINUS, "-", null, 1),
+                        new Expression.Literal(123)
+                ),
+                new Expression.Grouping(
+                        new Expression.Literal(45.67)
+                ),
+                new Token(TokenType.STAR, "*", null, 1)
+        );
+        AstPrinter astPrinter = new AstPrinter();
+        System.out.println(astPrinter.print(expression));
     }
 }
