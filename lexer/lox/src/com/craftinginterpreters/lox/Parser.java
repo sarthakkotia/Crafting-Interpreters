7f5043c473f7c5f0016731a0cf5888f3ba483264
Sarthak_Kotia <sarthakkotia@gmail.com>
2025-02-12 19:14:42 +0530

    replaced my version with the book version of the code which is more efficient

--- lexer/lox/src/com/craftinginterpreters/lox/Parser.java
+++ lexer/lox/src/com/craftinginterpreters/lox/Parser.java
@@ -33,47 +33,55 @@ public class Parser {
         return unary;
     }
     private Expression unary(){
-        ArrayList<TokenType> operators = new ArrayList<>();
-        operators.add(TokenType.BANG);
-        operators.add(TokenType.MINUS);
-        int index = match(operators);
-        if(index != -1){
-            Token operator = getToken();
-            advance();
-            return new Expression.Unary(operator, unary());
+        if(match(TokenType.BANG, TokenType.MINUS)){
+            Token operator = previous();
+            Expression right = unary();
+            return new Expression.Unary(operator, right);
         }
         return primary();
     }
     private Expression primary(){
-        Token present = getToken();
-        Expression expression = null;
-        if(present.type == TokenType.NUMBER || present.type == TokenType.STRING){
-            return new Expression.Literal(present.lexeme);
-        }else if(present.type == TokenType.TRUE){
-            return new Expression.Literal("true");
-        }else if(present.type == TokenType.FALSE){
-            return new Expression.Literal("false");
-        }else if(present.type == TokenType.NIL){
-            return new Expression.Literal("nil");
-        }else if(present.type == TokenType.LEFT_PAREN){
-            // get all the expression from it until right paren is not got
-            while(getToken().type != TokenType.RIGHT_PAREN){
+        if (match(TokenType.FALSE)) return new Expression.Literal(false);
+        if (match(TokenType.TRUE)) return new Expression.Literal(true);
+        if (match(TokenType.NIL)) return new Expression.Literal(null);
+        if (match(TokenType.NUMBER, TokenType.STRING)){
+            return new Expression.Literal(previous().literal);
+        }
+        if(match(TokenType.LEFT_PAREN)){
+            Expression expression = expression();
+//            consume(TokenType.RIGHT_PAREN)
+            while(peek().type != TokenType.RIGHT_PAREN){
                 expression = expression();
             }
         }
         return (expression == null) ? new Expression.Literal("") : expression;
     }
-    private int match(ArrayList<TokenType> operators){
-        TokenType present = getToken().type;
-        return operators.indexOf(present);
+    private Token previous(){
+        if(current == 0) Lox.error(-1, "previous character not available");
+        return tokens.get(current-1);
     }
-    private Token getToken(){
-        if(current == tokens.size()) Lox.error(-1, "");
+    private boolean match(TokenType... operators){
+        for(TokenType type: operators){
+            if(check(type)){
+                advance();
+                return true;
+            }
+        }
+        return false;
+    }
+    private boolean check(TokenType type){
+        if(isAtEnd()) return false;
+        return peek().type == type;
+    }
+    private boolean isAtEnd(){
+        return peek().type == TokenType.EOF;
+    }
+    private Token peek(){
         return tokens.get(current);
     }
-    private void advance(){
-        if(current == tokens.size()) Lox.error(1, "");
-        current++;
+    private Token advance(){
+        if(!isAtEnd()) current++;
+        return previous();
     }
 
     public static void main(String[] args) {
