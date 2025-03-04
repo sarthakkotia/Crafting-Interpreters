608e7b7fa5bd09aa908e54b7d5b5a200b0a3b837
Sarthak_Kotia <sarthakkotia@gmail.com>
2025-03-04 22:12:02 +0530

    evaluated binary, unary expressions

--- lox/src/com/craftinginterpreters/lox/Interpreter.java
+++ lox/src/com/craftinginterpreters/lox/Interpreter.java
@@ -3,7 +3,36 @@ package com.craftinginterpreters.lox;
 public class Interpreter implements Expression.Visitor<Object> {
     @Override
     public Object visitBinaryExpression(Expression.Binary expression) {
+        Object left = evaluate(expression.left);
+        Object right = evaluate(expression.right);
+        switch (expression.operator.type){
+            case TokenType.MINUS:
+                return (double) left - (double) right;
+            case TokenType.STAR:
+                return (double) left * (double) right;
+            case TokenType.SLASH:
+                return (double) left / (double) right;
+            case TokenType.PLUS:
+                if(left instanceof Double && right instanceof Double) return (double) left + (double) right;
+                if(left instanceof String && right instanceof String) return (String) left + (String) right;
+                break;
+            case TokenType.GREATER:
+                return (double) left > (double) right;
+            case TokenType.GREATER_EQUAL:
+                return (double) left >= (double) right;
+            case TokenType.LESS:
+                return (double) left < (double) right;
+            case TokenType.LESS_EQUAL:
+                return (double) left <= (double) right;
+            case TokenType.BANG_EQUAL: return !isEqual(left, right);
+            case TokenType.EQUAL_EQUAL: return isEqual(left, right);
+        }
         return null;
+    }
+    private boolean isEqual(Object left, Object right){
+        if(left == null && right == null) return true;
+        if(left == null || right == null) return false;
+        return left.equals(right);
     }
 
     @Override
@@ -19,9 +48,14 @@ public class Interpreter implements Expression.Visitor<Object> {
             case TokenType.MINUS:
                 return -(double)rightAnswer;
             case TokenType.BANG:
-                return !isTruthy(right);
+                return !isTruthy(rightAnswer);
         }
         return null;
+    }
+    private boolean isTruthy(Object object){
+        if(object == null) return false;
+        if(object instanceof Boolean) return (boolean) object;
+        return true;
     }
 
     @Override
