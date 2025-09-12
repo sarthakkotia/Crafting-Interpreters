ab02ee18b1f033dfc029125d527f03da6c2848ed
Sarthak_Kotia <sarthakkotia@gmail.com>
2025-09-12 21:49:49 +0530

    started the work on scanner

--- clox/common.h
+++ clox/common.h
@@ -4,8 +4,6 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include <stddef.h>
-#include <stdlib.h>
-#include <stdio.h>
 
 #define DEBUG_TRACE_EXECUTION
 
--- clox/main.c
+++ clox/main.c
@@ -1,29 +1,85 @@
+#include <stdlib.h>
+#include <stdio.h>
+
 #include "common.h"
 #include "chunk.h"
 #include "debug.h"
 #include "vm.h"
 
-int main(int argc, const char* argv[]) {
-    initVM();
-    Chunk chunk;
-    initChunk(&chunk);
-
-    if(argc > 2){
-        printf("Usage: jlox or jlox [script]");
-        exit(1);
-    }else if(argc == 2){
-        //runfile
-        printf("script\n");
-    }else{
-        printf("repl\n");
-        //run
+static char *readFile(const char *filePath){
+    FILE *filePtr = fopen(filePath, "rb");
+    if(filePtr == NULL){
+        fprintf(stderr, "Could not open file \" %s \". \n", filePath);
+        exit(74);
     }
 
-    interpret(&chunk);
+    fseek(filePtr, 0L, SEEK_END);
+    size_t fileSize = ftell(filePtr);
+    rewind(filePtr);
+
+    char *buffer = (char *) malloc(fileSize + 1);
+    if(buffer == NULL){
+        fprintf(stderr, "Not enough memory to read \" %s \". \n", filePath);
+        exit(74);
+    }
+    size_t bytesRead = fread(buffer, sizeof(char), fileSize, filePtr);
+    if(bytesRead < fileSize){
+        fprintf(stderr, "Could not read file \" %s \". \n", filePath);
+        exit(74);
+    }
+    buffer[bytesRead] = '\0';
+
+    fclose(filePtr);
+    return buffer;
+}
+
+static void runFile(const char *file_path){
+    char *source = readFile(file_path);
+    InterpretResult result = interpret(source);
+    free(source);
+
+    if(result == INTERPRET_COMPILE_ERROR) exit(65);
+    if(result == INTERPRET_RUNTIME_ERROR) exit(70);
+
+}
+
+static void repl(){
+#define MAX_LENGTH 1024
+
+    char line[MAX_LENGTH];
+    while(true){
+        printf("> ");
+        if(fgets(line, sizeof(line), stdin) != NULL){
+            //run line
+            interpret(line);
+        }else{
+            printf("\n");
+            break;
+        }
+    }
+#undef MAX_LENGTH
+}
+
+
+int main(int argc, const char* argv[]) {
+    initVM();
+//    Chunk chunk;
+//    initChunk(&chunk);
+
+    if(argc > 2){
+        fprintf(stderr, "Usage clox [path]\n");
+        exit(64);
+    }else if(argc == 2){
+        runFile(argv[1]);
+    }else{
+        repl();
+    }
+
+//    interpret(&chunk);
 
 //    disassembleChunk(&chunk, "test");
 
     freeVM();
-    freeChunk(&chunk);
+//    freeChunk(&chunk);
     return 0;
 }
