f7fdea068c2eb1138d5017093b01f86f9bb276f9
Sarthak_Kotia <sarthakkotia@gmail.com>
2025-09-15 21:40:12 +0530

    added tokens and their scanning logic

--- clox/compiler.c
+++ clox/compiler.c
@@ -1,4 +1,6 @@
+#include <stdio.h>
 #include "compiler.h"
+#include "scanner.h"
 
 
 void compile(const char *source){
@@ -6,4 +8,17 @@ void compile(const char *source){
     // converting it to bytecode
     // save them into chunks
     // save those chunks into bytecode
+    int line = -1;
+    for(;;){
+        Token token = scanToken();
+        if(token.line != line){
+            printf("%4d", token.line);
+            line = token.line;
+        }else{
+            printf("   | ");
+        }
+        printf("%2d '%.*s'\n", token.type, token.length, token.start);
+
+        if(token.type == EOF) break;
+    }
 }
--- clox/scanner.h
+++ clox/scanner.h
@@ -7,6 +7,28 @@ typedef struct {
     int line;
 }Scanner;
 
+typedef enum{
+    // Single character tokens
+    LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE, COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR, BREAK,
+    // One/Two character tokens
+    BANG, BANG_EQUAL, EQUAL, EQUAL_EQUAL, GREATER, GREATER_EQUAL, LESS, LESS_EQUAL,
+    // Literals
+    IDENTIFIER, STRING, NUMBER,
+    // Keywords
+    AND, CLASS, IF, ELSE, TRUE, FALSE, FUN, FOR, NIL, OR, PRINT, RETURN, SUPER, THIS, VAR, WHILE,
+    EOF
+}TokenType;
+
+typedef struct {
+    TokenType type;
+    const char *start;
+    int length;
+    int line;
+}Token;
+
+
+
 void initScanner(const char *source);
+Token scanToken();
 
 #endif //clox_scanner_h
