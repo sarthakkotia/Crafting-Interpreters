6806a57750095a37bde3ab781402c52f5b17d4d3
Sarthak_Kotia <sarthakkotia@gmail.com>
2025-09-25 22:18:02 +0530

    Chapter 16 completed, Scanner complete

--- clox/scanner.c
+++ clox/scanner.c
@@ -80,6 +80,12 @@ static void skipWhitespace(){
     }
 }
 
+static TokenType checkKeyword(int start, int length, const char *reference, TokenType type){
+    if(scanner.current - scanner.start == start + length && memcmp(scanner.start + start, reference, length) == 0)
+        return type;
+    return TOKEN_IDENTIFIER;
+}
+
 static Token handleString(){
     char ch = peek();
     while(ch != '"' && !isAtEnd()){
@@ -110,6 +116,43 @@ static bool isAlpha(char c){
 }
 
 static TokenType identifierType(){
+    switch (scanner.start[0]) {
+        case 'a': checkKeyword(1, 2, "nd", TOKEN_AND);
+        case 'c': checkKeyword(1, 4, "lass", TOKEN_CLASS);
+        case 'e': checkKeyword(1, 3, "lse", TOKEN_ELSE);
+        case 'i': checkKeyword(1, 1, "f", TOKEN_IF);
+        case 'n': checkKeyword(1, 2, "il", TOKEN_NIL);
+        case 'o': checkKeyword(1, 1, "r", TOKEN_OR);
+        case 'p': checkKeyword(1, 4, "rint", TOKEN_PRINT);
+        case 'r': checkKeyword(1, 5, "eturn", TOKEN_RETURN);
+        case 's': checkKeyword(1, 4, "uper", TOKEN_SUPER);
+        case 'v': checkKeyword(1, 2, "ar", TOKEN_VAR);
+        case 'w': checkKeyword(1, 4, "hile", TOKEN_WHILE);
+
+        case 'f':
+            if(scanner.current - scanner.start > 1){
+                switch (scanner.start[1]) {
+                    case 'a': checkKeyword(2, 3, "lse", TOKEN_FALSE);
+                    case 'o': checkKeyword(1, 1, "r", TOKEN_FOR);
+                    case 'u': checkKeyword(1, 1, "n", TOKEN_FUN);
+                }
+            }if(scanner.current - scanner.start > 1){
+                switch (scanner.start[1]) {
+                    case 'a': checkKeyword(2, 3, "lse", TOKEN_FALSE);
+                    case 'o': checkKeyword(1, 1, "r", TOKEN_FOR);
+                    case 'u': checkKeyword(1, 1, "n", TOKEN_FUN);
+                }
+            }
+            break;
+        case 't':
+            if(scanner.current - scanner.start > 1){
+                switch (scanner.start[1]) {
+                    case 'h': checkKeyword(2, 2, "is", TOKEN_THIS);
+                    case 'r': checkKeyword(1, 2, "ue", TOKEN_TRUE);
+                }
+            }
+            break;
+    }
     return TOKEN_IDENTIFIER;
 }
 
