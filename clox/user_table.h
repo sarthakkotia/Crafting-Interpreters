b4e245150e965f3488ed025f2aa24af163b30695
sarthakkotia <sarthakkotia@gmail.com>
2026-01-07 22:30:40 +0530

    initial setup

---
+++ clox/user_table.h
@@ -0,0 +1,18 @@
+#ifndef CLOX_USER_TABLE_H
+#define CLOX_USER_TABLE_H
+
+#include "value.h"
+
+typedef struct {
+    Value *key;
+    Value value;
+} Entry;
+
+typedef struct {
+    int count;
+    int capacity;
+    Entry *entries;
+} user_table;
+
+
+#endif //CLOX_USER_TABLE_H
