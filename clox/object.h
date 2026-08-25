479ae8591953284f0dcfa04d4cfb9d08617b82e4
sarthakkotia <sarthakkotia@gmail.com>
2026-08-25 22:53:49 +0530

    add value as argument name inIS_ check macros

--- clox/object.h
+++ clox/object.h
@@ -17,14 +17,14 @@
 #define AS_CSTRING(value) \
     (((ObjString *)AS_OBJ(value))->characters)
 
-#define IS_FUNCTION(object) \
-    (isObjType(object, OBJ_FUNCTION))
+#define IS_FUNCTION(value) \
+    (isObjType(value, OBJ_FUNCTION))
 
-#define IS_NATIVE(object) \
-    (isObjType(object,OBJ_NATIVE))
+#define IS_NATIVE(value) \
+    (isObjType(value,OBJ_NATIVE))
 
-#define IS_CLOSURE(object) \
-    (isObjType((object, OBJ_CLOSURE)))
+#define IS_CLOSURE(value) \
+    (isObjType((value, OBJ_CLOSURE)))
 
 #define AS_FUNCTION(value) \
     ((ObjFunction *)AS_OBJ(value))
@@ -96,8 +96,8 @@ ObjString* copyString(const char *characters, int length);
 ObjUpvalue* newUpvalue(Value *slot);
 void printObject(Value value);
 
-static inline bool isObjType(Value object, ObjectType type) {
-    return (IS_OBJ(object) && AS_OBJ(object)->type == type);
+static inline bool isObjType(Value value, ObjectType type) {
+    return (IS_OBJ(value) && AS_OBJ(value)->type == type);
 }
 
 #endif //CLOX_OBJECT_H
